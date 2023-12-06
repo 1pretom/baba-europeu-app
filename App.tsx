c57c4c2c78470f73f995d5996fd9cc4d9e152a90
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-06 09:29:42 -0300

    add theme provider

--- App.tsx
+++ App.tsx
@@ -3,14 +3,18 @@ import React from "react";
 import { StatusBar } from "expo-status-bar";
 import * as S from "./src/Screens/PlayersList/styles";
 import { Routes } from "@routes/index";
+import { ThemeProvider } from "styled-components";
+import theme from "src/theme";
 
 
 export default function App() {
   return (
-    <S.Container>
-      <StatusBar style="auto" />
+    <ThemeProvider theme={theme}>
+      <S.Container>
+        <StatusBar style="auto" />
 
-      <Routes />
-    </S.Container>
+        <Routes />
+      </S.Container>
+    </ThemeProvider>
   );
 }
