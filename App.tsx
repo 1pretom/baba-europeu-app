fcae32032c33de9462bb204e10fbead855729787
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-06 09:35:27 -0300

    fix import to theme

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StatusBar } from "expo-status-bar";
 import * as S from "./src/Screens/PlayersList/styles";
 import { Routes } from "@routes/index";
 import { ThemeProvider } from "styled-components";
-import theme from "src/theme";
+import theme from "./src/theme";
 
 
 export default function App() {
