7797a61360783b8f41e03a7dac6cea27c42b5439
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-09 21:06:52 -0300

    add icon to component header

--- src/components/Header/component.tsx
+++ src/components/Header/component.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import * as S from "./styles";
 import { TITLE_TEXT } from "./constants";
-//import { CarretLeft } from "phosphor-react-native";
+import { CarretLeft } from "phosphor-react-native";
 
 import babaLogo from "@assets/baba-logo.png";
 
 export const Header = () => {
   return (
     <S.Container>
-   {/* //   <CarretLeft/> */}
+   <CarretLeft/> 
       <S.Image source={babaLogo} />
       <S.Text>{TITLE_TEXT}</S.Text>
     </S.Container>
