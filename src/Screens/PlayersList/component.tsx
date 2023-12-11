d3f3957478d72020b92d958ea596918e8cefe5fb
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-11 00:04:24 -0300

    add highlight component

--- src/Screens/PlayersList/component.tsx
+++ src/Screens/PlayersList/component.tsx
@@ -4,10 +4,12 @@ import { Header } from "../../components/Header";
 import { Loading } from "../../components/ActivityIndicator/component";
 import { NextFut } from "../../components/NextFut";
 import * as S from "./styles";
+import { Highlight } from "@components/Highlight";
 
 export const PlayersList = () => {
   return (
     <S.Container>
+      <Highlight title="Times" subtitle="Adicione os jogadores" />
       <NextFut />
 
       <StatusBar style="auto" />
