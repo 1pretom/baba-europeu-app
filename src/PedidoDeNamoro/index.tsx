214f581fd76b0f5153b23c8a6726e501f6797882
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-11 09:57:38 -0300

    to be fixed

--- src/PedidoDeNamoro/index.tsx
+++ src/PedidoDeNamoro/index.tsx
@@ -1,2 +1,58 @@
 //faz o não, não ser clicável
 //fazer o sim ser a única opção possível e direcioar pra um vídeo
+
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Linking } from "react-native";
+
+const PedidoDeNamoro = () => {
+  const [noButtonPosition, setNoButtonPosition] = useState({
+    top: 50,
+    left: 50,
+  });
+
+  const handleNoButtonClick = () => {
+    const newTop = Math.random() * 500; // Gerar uma posição top aleatória
+    const newLeft = Math.random() * 300; // Gerar uma posição left aleatória
+    setNoButtonPosition({ top: newTop, left: newLeft });
+  };
+
+  const handleSimButtonClick = () => {
+    // Abrir um link para um vídeo (substitua o URL pelo seu link desejado)
+    Linking.openURL("https://www.youtube.com/watch?v=SEF0-U5KxW4");
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          padding: 20,
+          borderRadius: 10,
+          position: "absolute",
+          top: noButtonPosition.top,
+          left: noButtonPosition.left,
+        }}
+        onPress={handleNoButtonClick}
+      >
+        <Text style={{ color: "white" }}>Não</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "green",
+          padding: 20,
+          paddingTop: 20,
+
+          borderRadius: 10,
+          margin: 10,
+          marginTop: 10,
+        }}
+        onPress={handleSimButtonClick}
+      >
+        <Text style={{ color: "white" }}>Sim</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default PedidoDeNamoro;
--- src/Screens/Finances/component.tsx
+++ src/Screens/Finances/component.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from "react-native";
+import PedidoDeNamoro from "../../PedidoDeNamoro";
 
 export const Finances = () => {
   return (
     <View>
       <Text>Finances</Text>
+      <PedidoDeNamoro/>
+
     </View>
   );
 };
