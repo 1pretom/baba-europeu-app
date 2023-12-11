8993db6c0dfe26c0da3d195304fc869ac0bc7991
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-11 09:35:50 -0300

    add flatlist

--- src/Screens/ClassicScreen/index.tsx
+++ src/Screens/ClassicScreen/index.tsx
@@ -1,11 +1,27 @@
 import { ClassicCard } from "@components/ClassicCard";
 import * as S from "./styles";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { Highlight } from "@components/Highlight";
 
 export const ClassicScreen = () => {
+  const [classicTeam, setClassicTeam] = useState<string[]>(["Cahia", "Leão"]);
+
   return (
     <S.Container>
-      <ClassicCard title="Cahia" />
-      <ClassicCard title="Leão" />
+      <Highlight title="Dia de clássico" subtitle="Bote se vc é barril" />
+
+      <FlatList
+        data={classicTeam}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => {
+          console.log("Item:", item);
+          return <ClassicCard title={item} />;
+        }}
+      />
+
+      {/* <ClassicCard title="Cahia" />
+      <ClassicCard title="Leão" /> */}
     </S.Container>
   );
 };
