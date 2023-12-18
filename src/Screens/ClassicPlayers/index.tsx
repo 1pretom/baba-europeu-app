6e690a9b79ac6596355a41e21215967b5fed327d
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-18 10:04:20 -0300

    create flashlist to show teams

--- src/Screens/ClassicPlayers/index.tsx
+++ src/Screens/ClassicPlayers/index.tsx
@@ -3,7 +3,12 @@ import * as S from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlashList } from "@shopify/flash-list";
+import { useState } from "react";
 export const ClassicPlayers = () => {
+  const [team, setTeam] = useState<string>();
+  const [players, setPlayers] = useState<string[]>();
+
   return (
     <S.Container>
       <Highlight
@@ -15,7 +20,23 @@ export const ClassicPlayers = () => {
         <Input placeholder="Nombre de la persona" autoCorrect={false} />
         <ButtonIcon icon="add" type="PRIMARY" />
       </S.Fomr>
-      <Filter title={"Time A"}/>
+      <S.HeaderList>
+        <FlashList
+          data={["Time A", "Time B"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <S.NumbersOfPlayers>{players?.length}</S.NumbersOfPlayers>
+      </S.HeaderList>
+
+      <Filter title={"Time A"} />
     </S.Container>
   );
 };
--- src/Screens/ClassicPlayers/styles.ts
+++ src/Screens/ClassicPlayers/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +12,19 @@ export const Fomr = styled.View`
   flex-direction: row-reverse;
   justify-content: center;
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumbersOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
 `;
