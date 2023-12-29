226239a61d4645ef3570ce2edfe27ff24f460811
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2023-12-29 00:03:06 -0300

    add native base lib

--- App.tsx
+++ App.tsx
@@ -11,20 +11,23 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/ActivityIndicator";
 import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <ThemeProvider theme={theme}>
-      <S.Container>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        {fontsLoaded ? <Routes /> : <Loading />}
-      </S.Container>
-    </ThemeProvider>
+    <NativeBaseProvider>
+      <ThemeProvider theme={theme}>
+        <S.Container>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          {fontsLoaded ? <Routes /> : <Loading />}
+        </S.Container>
+      </ThemeProvider>
+    </NativeBaseProvider>
   );
 }
