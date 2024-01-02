48b602c00c9c0bd609ce7fb7b30bed4a063c6967
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-01-01 22:11:45 -0300

    add pivo to position

--- src/Screens/Signup/constants.ts
+++ src/Screens/Signup/constants.ts
@@ -11,13 +11,15 @@ export const HIGHLIGHT_SUBTITLE = "Faça sua conta";
 export const DATE_OF_BIRTH_PLACEHOLDER = "Data de Nascimento";
 
 export const POSITION_PICKER_ITEMS = [
-    { label: "Gol", value: "Goleiro" },
-    { label: "Meia", value: "Meia" },
+    { label: "Goleiro", value: "Goleiro" },
     { label: "Defesa", value: "Defesa" },
     { label: "Fixo", value: "Fixo" },
     { label: "Lateral", value: "Lateral" },
     { label: "Volante", value: "Volante" },
+    { label: "Meia", value: "Meia" },
     { label: "Ala", value: "Ala" },
     { label: "Ponta", value: "Ponta" },
     { label: "Atacante", value: "Atacante" },
+    { label: "Pivô", value: "Pivô" },
+
   ];
