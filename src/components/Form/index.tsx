80ee9b62a707461acdb3d12a941de51ec64f8c7e
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-01-21 15:05:24 -0300

    start creation of Form component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@components/Input";
+import { Container } from "styles";
+
+export const Form = () => {
+  return (
+    <Container>
+      <Input />
+    </Container>
+  );
+};
---
+++ src/components/Form/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  width: 100%;
+  border-radius: 6px;
+  padding: 16px;
+`;
