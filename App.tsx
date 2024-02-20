e3279f74e7e682c4f1d2076012d7c8e6df87c6cc
Washington Conceição <89045117+1pretom@users.noreply.github.com>
2024-02-20 17:50:36 -0300

    initial add tracks

--- App.tsx
+++ App.tsx
@@ -11,19 +11,26 @@ import {
 } from "@expo-google-fonts/roboto";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { Mixpanel } from "mixpanel-react-native";
+
+const trackAutomaticEvents = false;
+Mixpanel.init("", trackAutomaticEvents);
+
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-      <ThemeProvider theme={theme}>
-        <S.Container>
-          <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-          />
-          {fontsLoaded ? <Routes /> : <Loading />}
-        </S.Container>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <S.Container>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </S.Container>
+    </ThemeProvider>
   );
 }
